cd92a51a827317adb6b100ecaa2dce1d760b6400
dugustlr@naver.com <dugustlr@naver.com>
2024-07-27 23:52:36 +0900

    WebSocket에서 서버와 브라우저를 연결하는 방법
    - WebSocket도 http와 같은 일종의 통신 프로토콜인데, 차이점은 statless한 http와는 달리 WebSocket은 유저를 기억하고 있다는 것. 유저로부터 요청이 올 때만 응답해주는 수동적인 http 방식과는 달리 WebSocket은 유저 요청을 기다릴 필요 없이 서버 측에서 먼저 메시지를 보낼 수 있음. 단, 서버와 브라우저가 '연결' 상태가 되어 있다는 가정 하에...
    
    [+]
    - 'ws' dependency 추가

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
+import http from "http";
 import path from "path";
+import ws from "ws";
 
 const app = express();
 
@@ -11,4 +13,15 @@ app.get("/", (req: Request, res: Response) => {
   res.render("home");
 });
 
-app.listen(3000, () => console.log("3000번 포트 연결 중..."));
+app.get("/*", (req: Request, res: Response) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new ws.WebSocketServer({ server });
+
+// 특정 이벤트가 발생하는 것을 기다렸다가, 이벤트가 발생하면 콜백함수 실행시켜주는 거임
+wss.on("connection", (socket) => {
+  // 여기서 socket이 의미하는 것은 연결된 대상(브라우저) 또는 연결된 대상과의 연락 라인
+  console.log(socket);
+});
+
+server.listen(3000, () => console.log("3000번 포트 연결 중..."));
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,4 @@
-console.log("hihihi");
+// 여기서의 socket은 서버로의 연결을 뜻함
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+console.log(socket);
