56b99b4384a00e67a44b8692f5b24fe009ea38ff
dugustlr@naver.com <dugustlr@naver.com>
2024-08-09 14:43:16 +0900

    음소거 및 카메라 on/off 기능 추가
    [+]
    - 메인화면에 카메라 선택 select 박스 추가

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,6 @@
 const socket = io();
 
+const cametaSelect = document.querySelector("select#camera_select");
 const myCam = document.querySelector("video#my_cam");
 const myControl = document.querySelector("div#my_control");
 const myMuteButton = myControl.querySelector("button.mute");
@@ -9,6 +10,24 @@ let myStream;
 let isMuted = false;
 let isCameraOff = false;
 
+async function getCameras() {
+  try {
+    // 유저에게 연결된 모든 디바이스들의 정보를 불러오는 거임
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((item) => item.kind === "videoinput");
+
+    cameras.forEach((item) => {
+      const option = document.createElement("option");
+
+      option.value = item.deviceId;
+      option.innerText = item.label;
+      cametaSelect.append(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
@@ -17,12 +36,15 @@ async function getMedia() {
     });
 
     myCam.srcObject = myStream;
+    getCameras();
   } catch (error) {
     console.log(error);
   }
 }
 
 myMuteButton.addEventListener("click", () => {
+  myStream.getAudioTracks().forEach((item) => (item.enabled = !item.enabled));
+
   if (isMuted) {
     isMuted = false;
     myMuteButton.innerText = "마이크 on";
@@ -33,6 +55,8 @@ myMuteButton.addEventListener("click", () => {
 });
 
 myCameraButton.addEventListener("click", () => {
+  myStream.getVideoTracks().forEach((item) => (item.enabled = !item.enabled));
+
   if (isCameraOff) {
     isCameraOff = false;
     myCameraButton.innerText = "카메라 on";
